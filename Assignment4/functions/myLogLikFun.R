ccfef14ff1b80232393df2663cb3c738810233bf
NiklasGerds <s224357@student.dtu.dk>
2025-05-02 11:59:23 +0200

    f

--- Assignment4/functions/myLogLikFun.R
+++ Assignment4/functions/myLogLikFun.R
@@ -6,8 +6,8 @@ myLogLikFun <- function(theta, y, R, x_prior = 0, P_prior = 10) {
   kf_result <- myKalmanFilter(y,theta = c(a,b,sigma1),R,x_prior = x_prior,P_prior = P_prior)# call the Kalman filter function
   err <- kf_result$innovation       # Innovations
   S <- kf_result$innovation_var   # Innovation covariances
-  
   # Compute log-likelihood contributions from each time step
-  logL <- -1/2 * sum(log(innovation_var)+innovation*(innovation_var)^(-1)*innovation)
+
+  logL <- -1/2 * sum(log(S)+err^2/S)
   return(-logL)  # Return negative log-likelihood for minimization
 }
